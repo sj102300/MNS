ff9ba8ac36fbb45aac32e45316cd351018de2692
LeeSeungYeop <tyler980307@gmail.com>
2025-05-13 15:30:51 +0900

    MAS-24 fix(GET): 시나리오 목록 요청 속도 개선

--- OCC/obj/Release/net8.0-windows/OCC.AssemblyInfo.cs
+++ OCC/obj/Release/net8.0-windows/OCC.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("OCC")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+becfa9299e0593f0c8eefc1fa0ccfaea5f73166c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OCC")]
--- SCN/SCN.h
+++ SCN/SCN.h
@@ -11,3 +11,6 @@ void handle_post(web::http::http_request request);
 
 // GET 요청 처리 함수 선언
 void handle_get(web::http::http_request request);
+
+// 시나리오 목록 캐싱 함수
+void load_scenario_meta_cache(const std::string& directory_path);
