3b1dd49d2fc3445946dfd14d4c1229d90fcc85b7
seungJoo <sj10230506@gmail.com>
2025-05-14 09:56:10 +0900

    MAS-22 chore(AircraftManager): ip주소 변경

--- TCC/main.cpp
+++ TCC/main.cpp
@@ -2,10 +2,20 @@
 
 #include "AircraftManager.h"
 
+#define DEVELOP 1
+
+#if DEVELOP
+#define AIRCRAFT_SENDER_IP "192.168.2.7"
+
+#else 
+#define AIRCRAFT_SENDER_IP "127.0.0.1"
+
+#endif
+
 int main() {
 
     IAircraftReceiver* aircraftReceiver = new AircraftReceiver(std::string("239.0.0.2"), 9999);
-    IAircraftSender* aircraftSender = new AircraftSender(std::string("127.0.0.1"), 8081);
+    IAircraftSender* aircraftSender = new AircraftSender(std::string(AIRCRAFT_SENDER_IP), 9000);
     AircraftManager aircraftmanager(aircraftReceiver, aircraftSender);
 
     aircraftmanager.updateAircraftPosition();
