44d7be9913e6d7416f1008dbb14f2faffe3647a4
LeeSeungYeop <tyler980307@gmail.com>
2025-05-15 14:30:48 +0900

    MAS-20 chore: obj 파일 삭제

--- OCC/obj/Release/net8.0-windows/OCC.AssemblyInfo.cs
+++ OCC/obj/Release/net8.0-windows/OCC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OCC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd568f6143db2c233d18602ec7274a36d53bcac9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bc5b2a0f52ecffac24263ecf271034adf128b39")]
 [assembly: System.Reflection.AssemblyProductAttribute("OCC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OCC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
